52ffb23dcf837cf6818641998e8a12568bc23bd2
예빈 <yebin060310@gmail.com>
2023-10-12 12:39:10 +0900

    Update img

--- src/components/Webcam/index.tsx
+++ src/components/Webcam/index.tsx
@@ -1,7 +1,11 @@
-import { useRef, useState, useCallback } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
 import Webcam from "react-webcam";
 import * as S from "./style";
 import Image from "next/image";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import * as I from "assets";
+
 const videoConstraints = {
   width: 1520,
   height: 860,
@@ -10,53 +14,122 @@ const videoConstraints = {
 
 const WebcamComponent = () => {
   const webcamRef = useRef<Webcam>(null);
-  const [url, setUrl] = useState<string | null>(null);
-  const capture = useCallback(() => {
+  const [capturedImages, setCapturedImages] = useState<string[]>([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [imageTitle, setImgTitle] = useState("첫번째");
+  const { replace } = useRouter();
+
+  const capture = useCallback(async () => {
     const imageSrc = webcamRef.current?.getScreenshot();
     if (imageSrc) {
-      setUrl(imageSrc);
+      setCapturedImages((prevImages) => {
+        const updatedImages = [...prevImages];
+        updatedImages[currentImageIndex] = imageSrc;
+        return updatedImages;
+      });
+
+      if (currentImageIndex < 3) {
+        setCurrentImageIndex(currentImageIndex + 1);
+      }
+      if (currentImageIndex < 4) {
+        switch (currentImageIndex) {
+          case 0:
+            setImgTitle("첫번째");
+            break;
+          case 1:
+            setImgTitle("두번째");
+            break;
+          case 2:
+            setImgTitle("세번째");
+            break;
+          case 3:
+            setImgTitle("네번째");
+            break;
+          default:
+            setImgTitle("");
+        }
+      }
     }
-  }, [webcamRef]);
+  }, [webcamRef, currentImageIndex]);
+
+  const resetCapture = () => {
+    setCapturedImages([]);
+    setCurrentImageIndex(0);
+  };
+
+  const sendImages = async () => {
+    if (capturedImages.length < 4) {
+      console.error("Not enough images to send.");
+      return;
+    }
+
+    const formData = new FormData();
+    for (let i = 0; i < 4; i++) {
+      if (capturedImages[i]) {
+        const blobImage = await fetch(capturedImages[i]).then((response) =>
+          response.blob()
+        );
+        formData.append(`image${i}`, blobImage, `image${i}.jpg`);
+      }
+    }
+
+    try {
+      const response = await axios.post(
+        "http://3.34.22.93:8080/v2/image/upload/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("Response:", response);
+
+      if (response.status === 200) {
+        console.log("Images uploaded successfully!");
+        replace("/frame");
+      } else {
+        console.error("Failed to upload images.");
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+    }
+  };
 
   return (
     <S.WebcamComponent>
-      <header>
-        <h1>첫번째 포즈 : ~~~~~~~</h1>
-      </header>
-      <>
-        <div>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-          />
-        </div>
-        <S.Button onClick={capture}>사진 찍기</S.Button>
-      </>
-      {url && (
-        <>
-          <div>
-            <S.Button
-              onClick={() => {
-                setUrl(null);
-              }}
-            >
-              다시 찍기
-            </S.Button>
-            <S.Button
-              onClick={() => {
-                setUrl(null);
-              }}
-            >
-              다음
-            </S.Button>
-          </div>
-          <S.CaptureImg>
-            <Image width={200} height={100} src={url} alt="Screenshot" />
-          </S.CaptureImg>
-        </>
-      )}
+      <S.TitleBox>
+        <S.Title>{imageTitle} 포즈</S.Title>
+        <S.Text>두 손을 모아 하트 모양을 만들어 보세요 !</S.Text>
+      </S.TitleBox>
+      <div>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
+      </div>
+      <S.ButtonContainer>
+        <S.Text onClick={resetCapture}>다시 찍기</S.Text>
+        <S.CameraButton onClick={capture}>
+          <I.CameraIcon />
+        </S.CameraButton>
+        <S.Text onClick={sendImages}>사진 보내기</S.Text>
+      </S.ButtonContainer>
+      <S.CaptureImg>
+        {capturedImages.map((image, index) => (
+          <S.ImgContainer key={index}>
+            <Image
+              width={200}
+              height={100}
+              src={image}
+              alt={`Screenshot ${index}`}
+            />
+          </S.ImgContainer>
+        ))}
+      </S.CaptureImg>
     </S.WebcamComponent>
   );
 };
--- src/components/Webcam/style.ts
+++ src/components/Webcam/style.ts
@@ -7,23 +7,68 @@ export const WebcamComponent = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  background-color: pink;
+  background-color: black;
 `;
 
 export const CaptureImg = styled.div`
   position: absolute;
-  top: 900px;
-  left: 100px;
-  height: 100px;
-  width: 200px;
-`;
-
-export const Button = styled.div`
-  height: 30px;
-  width: 100px;
-  background-color: palevioletred;
+  top: 100px;
+  left: 0px;
+  width: 230px;
+  height: 430px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 12px;
+  background: #fff;
+  box-shadow: 0px 4px 30px 0px rgba(112, 144, 176, 0.08);
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 10px;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 408px;
+  height: 86px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CameraButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 86px;
+  height: 86px;
+  z-index: 10000000;
+  border-radius: 100%;
+  background-color: #ffffff;
+  cursor: pointer;
+`;
+
+export const ImgContainer = styled.div``;
+export const TitleBox = styled.div`
+  width: 408px;
+  height: 90px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+export const Title = styled.div`
+  color: #fff;
+  font-family: Inter;
+  font-size: 34px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const Text = styled.div`
+  color: rgba(255, 255, 255, 0.8);
+  font-family: Inter;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: normal;
+  cursor: pointer;
 `;
