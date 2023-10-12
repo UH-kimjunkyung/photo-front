848aa5650a6058ee2f7aeee80283e7183d75768e
예빈 <yebin060310@gmail.com>
2023-10-12 20:14:09 +0900

    Update ai btn

--- src/components/Aibutton/index.tsx
+++ src/components/Aibutton/index.tsx
@@ -7,17 +7,29 @@ import axios from "axios";
 import Image from "next/image";
 const AiButton: React.FC = () => {
   const { imgUrl } = useStore();
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState(Array(4).fill(""));
 
   const getAiImages = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
-        "http://3.34.22.93:8080/v2/image/upload/"
+        "https://snap.team-alt.com/v2/image/conversion?image=http://uh-alb-2127268112.ap-northeast-2.elb.amazonaws.com/v2/image/download/preview/4c1df73d-f486-4b17-b2d0-8bea9d822887&text=anime"
       );
 
       console.log("Response:", response);
 
       if (response.status === 200) {
-        console.log("Images uploaded successfully!");
+        setLoading(false);
+        alert("이미지 요청에 성공하였습니다");
+        for (let i = 0; i < 4; i++) {
+          setImages((prevImages) => {
+            const updatedImages = [...prevImages];
+            updatedImages[i] = response.data.data.images[i];
+            return updatedImages;
+          });
+        }
+        console.log(images);
       } else {
         console.error("Failed to upload images.");
       }
@@ -27,9 +39,14 @@ const AiButton: React.FC = () => {
   };
 
   return (
-    <S.AiButton onClick={getAiImages}>
-      <img src="/imgs/333.png" width={74} height={74} />
-    </S.AiButton>
+    <>
+      {loading && (
+        <S.AiButtonTitle>Ai 이미지로 변환하는 중이에요···</S.AiButtonTitle>
+      )}
+      <S.AiButton onClick={getAiImages}>
+        <img src="/imgs/333.png" width={74} height={74} />
+      </S.AiButton>
+    </>
   );
 };
 
